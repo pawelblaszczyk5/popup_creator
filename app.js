0aa008a6967a2f813f4a29d14df59ed1a9df1dd1
pawelblaszczyk5 <pawel.blaszczyk@wir.pl>
2020-08-10 16:51:16 +0200

    Started working on placement function

--- app.js
+++ app.js
@@ -1,36 +1,71 @@
-let place = (where, what, direction) => {
-
+const get_new_row = () => {
+    let div = document.createElement("div")
+    div.classList.add("row")
+    div.id = "row" + document.getElementById("popup").shadowRoot.getElementsByClassName("row")
+    return div
+}
+const place = (placement) => {
+    console.log(placement)
+    if (placement.direction != inside)
+        let row = get_new_row()
+    switch (placement.direction) {
+        case "over":
+            break;
+        case "inside":
+            break;
+        case "under":
+            break;
+        case "top":
+            break;
+        case "bottom":
+            break;
+    }
 }
 // calculate where item will be placed while moving it
-let placehold = (x, y) => {
-    let popup_offset_left = document.getElementById("popup").getBoundingClientRect().left
-    let popup_offset_top = document.getElementById("popup").getBoundingClientRect().top + 1
+const calculate_placement = (x, y) => {
     let shadowRoot = document.getElementById("popup").shadowRoot
     let content = shadowRoot.getElementById("content")
     let hovered_items = shadowRoot.querySelectorAll(':hover')
-    let where
-    let direction
     if (hovered_items.length > 0) {
-        var height = 0
+        let height = 0
         for (let element of content.children) {
-            console.log(element.getBoundingClientRect().top - parseInt(window.getComputedStyle(element).marginTop) - popup_offset_top, element.getBoundingClientRect().top + element.getBoundingClientRect().height + parseInt(window.getComputedStyle(element).marginBottom) - popup_offset_top)
             if (y > element.getBoundingClientRect().top - parseInt(window.getComputedStyle(element).marginTop) && y < element.getBoundingClientRect().top + element.getBoundingClientRect().height + parseInt(window.getComputedStyle(element).marginBottom)) {
-                console.log("jesteśmy w tym elemencie")
-                console.log(element)
-                if (y < 0.33 * element.getBoundingClientRect().top + element.getBoundingClientRect().height + parseInt(window.getComputedStyle(element).marginBottom)) {
-                    console.log("nad")
-                } else if (y < 0.66 * element.getBoundingClientRect().top + element.getBoundingClientRect().height + parseInt(window.getComputedStyle(element).marginBottom)) {
-                    console.log("w")
+                let element_height = element.getBoundingClientRect().height + parseInt(window.getComputedStyle(element).marginTop) + parseInt(window.getComputedStyle(element).marginBottom)
+                let y_offset = y - element.getBoundingClientRect().top + parseInt(window.getComputedStyle(element).marginTop)
+                if (y_offset < 0.33 * element_height) {
+                    return ({
+                        direction: "over",
+                        element: element
+                    })
+                } else if (y_offset < 0.66 * element_height) {
+                    return ({
+                        direction: "inside",
+                        element: element
+                    })
+                } else {
+                    return ({
+                        direction: "under",
+                        element: element
+                    })
                 }
-            } else
-                console.log(x - popup_offset_left, y - popup_offset_top)
-
+            }
         }
+        if (y < content.children[0].getBoundingClientRect().top) {
+            return ({
+                direction: "top"
+            })
+        } else {
+            return ({
+                direction: "bottom"
+            })
+        }
+
     }
 }
 // function to drag elements around
-let drag = function (e) {
+const drag = function (e) {
     let element = this.cloneNode(true);
+    let what = this
     document.body.appendChild(element)
     element.style.position = "absolute"
     element.style.left = e.clientX + "px"
@@ -39,23 +74,32 @@ let drag = function (e) {
     element.style.height = this.getBoundingClientRect().height + "px"
     element.style.borderColor = "black"
     element.style.color = "black"
-    let up = function (e) {
+
+    const up = function (e) {
         e.preventDefault();
         window.removeEventListener("mousemove", move)
         window.removeEventListener("mouseup", up)
         element.parentElement.removeChild(element)
     }
-    let move = function (e) {
+    const move = function (e) {
+
         e.preventDefault();
         element.style.left = e.clientX + 10 + "px"
         element.style.top = e.clientY + 10 + "px"
-        placehold(e.clientX, e.clientY)
+        calculate_placement(e.clientX, e.clientY)
+        let placement = calculate_placement(e.clientX, e.clientY)
+        if (placement) {
+            place({
+                ...placement,
+                what: what
+            })
+        }
     }
     window.addEventListener("mouseup", up)
 
     window.addEventListener("mousemove", move)
 }
-let initialize = () => {
+const initialize = () => {
     //generate list of draggable elements
     let tools = document.getElementById("tools")
     for (let element of document.getElementsByClassName("template")) {
@@ -101,12 +145,15 @@ let initialize = () => {
                     display: flex;
                     flex-direction: column;
                     align-items: center;
+                    padding: 15px;
                 }
                 .row{
                     width: 100%;
                     display: flex;
                     flex-direction: row;
                     height: 50px;
+                    border: 1px solid red;
+                    margin-top: 10px;
                 }
                 `;
             shadow.appendChild(style)
